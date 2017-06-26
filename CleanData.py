b0f7ac9c318e69bbcdac6f5ca9f6c76744ec6002
Tong ZHAN <zhantong1994@163.com>
2017-06-26 20:04:47 +0800

    训练集以JSON格式保存

--- CleanData.py
+++ CleanData.py
@@ -12,25 +12,32 @@ class CleanData:
         self.reg = re.compile(r'(.*?)\s\[(.*?)\]\s(.*?)\s(.*?)\s\[(.*?)\]\s\-\s(.*?)$')
         self.points = {}
         self.values = {}
-
-    def load_log(self, log_file_path, channels=[0]):
+    def format_log(self,log_file_path,channels=(0,),output_file_path='output.json'):
+        log_reg = re.compile(r'(.*?)\s\[(.*?)\]\s(.*?)\s(.*?)\s\[(.*?)\]\s\-\s(.*?)$')
+        result = {}
+        result['frames'] = []
+        result['file'] = {}
         with open(log_file_path, 'r', encoding='utf-8') as f:
             for line in f:
-                res = self.reg.findall(line)
+                res = log_reg.findall(line)
                 log_type = res[0][4]
-                if log_type == 'processed':
-                    log_data = json.loads(res[0][5])
-                    self.each_source(log_data, channels)
+                log_content = res[0][5]
+                if log_type == 'source':
+                    result['file']['path'] = log_content
+                elif log_type == 'barcodeConfig':
+                    result['barcodeConfig'] = json.loads(log_content)
+                elif log_type == 'processed':
+                    log_content=json.loads(log_content)
+                    log_content['learningData']=self.each_source(log_content,channels)
+                    result['frames'].append(log_content)
+        with open(output_file_path, 'w', encoding='utf-8') as f:
+            f.write(json.dumps(result))
 
     def each_source(self, data, channels):
-        index = data['image']['index']
         if 'barcode' not in data:
-            print('passed image', index)
-            return
+            return []
         samples_per_unit = data['barcode']['samplesPerUnit']
 
-        overlap_situation = data['barcode']['overlapSituation']
-        is_random = data['barcode']['isRandom']
         contents = [list(chunks(data['barcode']['samplePixels'][channel], samples_per_unit)) for channel in channels]
         contents = [sum(item, []) for item in zip(*contents)]
         real_points = list(chunks(data['barcode']['realSampleCoordinates'], samples_per_unit * 2))
@@ -42,14 +49,8 @@ class CleanData:
             current.extend(content)
             point = sample_points[0]
             current.extend([bar_1[point[1]], bar_2[point[1]]])
-            current.insert(0, overlap_situation)
-            current.insert(0, 1 if is_random else 0)
-            current.insert(0, index)
             packet.append(current)
-        self.points[index] = packet
-        if is_random and 'random' in data and 'truthValue' in data['random']:
-            self.values[index] = data['random']['truthValue']
-            print(index)
+        return packet
 
     def process_varybar(self, data):
         bar_1 = self.process_varybar_single(data[0])
@@ -65,28 +66,11 @@ class CleanData:
             varybar[keys[i]] = values[i]
         return varybar
 
-    def dump_to_file(self, with_class_file_path='data_with_value.txt',
-                     without_class_file_path='data_without_value.txt'):
-        with open(with_class_file_path, 'w', encoding='utf-8') as f_with_value \
-                , open(without_class_file_path, 'w', encoding='utf-8') as f_without_value:
-            for index, contents in sorted(self.points.items()):
-                if index in self.values:
-                    values = self.values[index]
-                    for content, value in zip(contents, values):
-                        content.append(value)
-                        line = ','.join(str(x) for x in content)
-                        f_with_value.write(line + '\n')
-                else:
-                    for content in contents:
-                        line = ','.join(str(x) for x in content)
-                        f_without_value.write(line + '\n')
-
 
 if __name__ == '__main__':
-    log_file_path = '2017-06-22 13-58-52.txt'
+    log_file_path = '2017-06-26 19-57-25.txt'
     barcode = CleanData()
     # Black White Barcode
-    barcode.load_log(log_file_path, [0])
+    barcode.format_log(log_file_path, (0,))
     # Color Barcode
-    # barcode.load_log(log_file_path,[1,2])
-    barcode.dump_to_file()
+    # barcode.load_log(log_file_path,(1,2))
